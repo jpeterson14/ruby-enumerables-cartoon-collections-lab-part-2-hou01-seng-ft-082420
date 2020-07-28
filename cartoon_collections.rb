1446019e9b63982d449ddab2d78fe14dfef286f9
Jake Peterson <jpeterson0514@gmail.com>
2020-07-28 23:58:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,4 +26,3 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do {|elements| elements == valid_calls}
 end
-end
